655d1607edd5e70244e5e5a38ca9fbef8d03b8f8
Cristian Sanchez <ederson12f@gmail.com>
2022-02-06 12:41:35 -0500

    3 basic routes of the application(home , login , register)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import { Home } from './pages/Home/index.jsx'
+import { Login } from './pages/Login/index.jsx'
+import { Register } from './pages/Register/index.jsx'
 
 export const App = () => {
-  return <h1>Hola mundo</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/Register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
